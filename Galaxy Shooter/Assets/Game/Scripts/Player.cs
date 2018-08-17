22159d96b66c0600d6d631328c2cda86d332c46d
Paul Kwak <pkwak1@nd.edu>
2018-08-16 20:07:20 -0400

    movement done

--- Galaxy Shooter/Assets/Game/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Game/Scripts/Player.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    // Serialization 
+    [SerializeField]
+    private float speed = 5.0f;
 
 	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello!");
+	private void Start () {
+        transform.position = new Vector3(0, 0, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log("Paul");
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
 	}
+
+    private void Movement() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(speed * horizontalInput * Time.deltaTime, speed * verticalInput * Time.deltaTime, 0));
+
+        // player bounds for x and y pos
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+        else if (transform.position.x > 11.5f)
+        {
+            transform.position = new Vector3(-11.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.5f)
+        {
+            transform.position = new Vector3(11.5f, transform.position.y, 0);
+        }
+    }
 }
